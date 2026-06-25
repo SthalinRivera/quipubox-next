970a1bdbba892dc31c5774d8bd7a89e78003e103
sthalin <sthalin.11@gmail.com>
2026-06-25 09:58:55 -0500

    test 4

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@/lib/supabase/server';
 import { NextResponse } from 'next/server';
-
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
     const requestUrl = new URL(request.url);
     const code = requestUrl.searchParams.get('code');
