361ab0a46910591b317e1afd97dd43e4b66b0fc9
sthalin <sthalin.11@gmail.com>
2026-06-25 09:41:59 -0500

    test 3

--- src/app/api/proxy/[...path]/route.ts
+++ src/app/api/proxy/[...path]/route.ts
@@ -1,6 +1,8 @@
+
+//app/api/proxy/[...path]/route.ts
 import { createClient } from '@/lib/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
-
+export const dynamic = 'force-dynamic';
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || '';
 
 export async function GET(
